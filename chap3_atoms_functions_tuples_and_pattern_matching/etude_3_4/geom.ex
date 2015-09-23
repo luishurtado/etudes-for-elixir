d70db7feee1b3a201da74e1cad45a4b6ece9931a
Luis Hurtado <luisfelipehurtado@gmail.com>
2015-09-23 16:00:00 -0500

    Étude 3-4: Tuples as Parameters

---
+++ chap3_atoms_functions_tuples_and_pattern_matching/etude_3_4/geom.ex
@@ -0,0 +1,32 @@
+defmodule Geom do
+  @moduledoc """
+  Provides geometric-related functions.
+
+  ## Examples
+
+    iex> Geom.area(:rectangle, 3, 4)
+    12
+
+    iex> Geom.area(:triangle, 3, 5)
+    7.5
+
+    iex> Geom.area(:ellipse, 2, 4)
+    25.132741228718345
+  """
+
+  @doc """
+  Calculates the area given a `shape`, `a` and `b`.
+  where `a` and `b` represent dimentions relevan to the `shape`.
+  """
+  def area({shape, a, b}), do: area(shape, a, b)
+
+  defp area(shape, a, b) do
+    case shape do
+      :rectangle when a >= 0 and b >= 0 -> a * b
+      :triangle  when a >= 0 and b >= 0 -> (a * b) / 2.0
+      :ellipse   when a >= 0 and b >= 0 -> :math.pi() * a * b
+      _ -> {:error, "invalid parameters"}
+    end
+  end
+
+end
